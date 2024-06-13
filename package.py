63290accf95873f85ecd13a8f0da4d256fd03254
stephenmyers <spmyers26@gmail.com>
2024-06-13 12:07:27 -0500

    adds logic to Package.status(), reformats Package.__init__()

--- package.py
+++ package.py
@@ -1,17 +1,21 @@
 # package.py
 
 class Package:
-    def __init__(self, package_id, address, deadline, city, zip_code, weight, load_time=None, delivery_time=None):
+    def __init__(self, package_id, address, city, zip_code, deadline, weight, load_time=None, delivery_time=None):
         self.package_id = package_id
         self.address = address
-        self.deadline = deadline
         self.city = city
         self.zip_code = zip_code
+        self.deadline = deadline
         self.weight = weight
         self.load_time = load_time
         self.delivery_time = delivery_time
 
     def status(self, time):
+        if self.load_time is None:
+            return 'is at hub'
+        if self.delivery_time is None:
+            return 'is en route'
         if self.delivery_time < time:
             return f'was delivered at {self.delivery_time}'
         elif self.load_time < time:
