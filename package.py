255edfbec98dbae5503425cc37b17dace399afe1
stephenmyers <spmyers26@gmail.com>
2024-06-17 14:07:18 -0500

    refactors package.status() to receive datetime.datetime argument

--- package.py
+++ package.py
@@ -12,16 +12,24 @@ class Package:
         self.delivery_time = delivery_time
 
     def status(self, time):
-        if self.load_time is None:
-            return 'is at hub'
-        if self.delivery_time is None:
-            return 'is en route'
-        if self.delivery_time < time:
-            return f'was delivered at {self.delivery_time}'
-        elif self.load_time < time:
-            return 'is en route'
+        if self.delivery_time[0] < time[0] or self.delivery_time[0] == time[0] and self.delivery_time[1] <= time[1]:
+            hour = self.delivery_time[0]
+            minute = self.delivery_time[1]
+            am_or_pm = 'AM'
+            zero_check = ''
+            if hour > 12:
+                hour = hour - 12
+                am_or_pm = 'PM'
+            if minute < 10:
+                zero_check = '0'
+            return f'was delivered. Delivery completed at {hour}:{zero_check}{minute} {am_or_pm}'
+        elif self.load_time[0] < time[0] or self.load_time[0] == time[0] and self.load_time[1] <= time[1]:
+            return 'was en route.'
         else:
-            return 'is at hub'
+            delayed_packages = [6, 25, 28, 32]
+            if self.package_id in delayed_packages:
+                return 'was delayed on flight.'
+            return 'was at hub.'
 
     def __str__(self, time):
         return f'Package {self.package_id} {self.status(time)}'
